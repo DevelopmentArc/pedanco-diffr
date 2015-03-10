4d2f46b14e01ce0e1b6f77abc82b727efd45c1db
James Polanco <james@developmentarc.com>
2015-03-10 11:55:21 -0700

    First commit
    * Created gemset
    * adding configurations and license

---
+++ lib/pedanco/diffr.rb
@@ -0,0 +1,8 @@
+# 3rd Party Gem Dependencies
+require 'active_support'
+require 'active_support/core_ext/object/blank'
+
+# Gem Dependencies
+require 'pedanco/diffr/version'
+require 'pedanco/diffr/change'
+require 'pedanco/diffr/change_set'
---
+++ lib/pedanco/diffr/version.rb
@@ -0,0 +1,5 @@
+module Pedanco
+  module Diffr
+    VERSION = '1.0.0'
+  end
+end
